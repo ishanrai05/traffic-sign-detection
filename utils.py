1e9152d6e34908e4295745a9fc74844c91bfa307
ishanrai05 <ishanrai05@gmail.com>
2019-08-08 15:51:27 +0530

    Adds utility method

---
+++ utils.py
@@ -0,0 +1,18 @@
+# this function is used during training process, to calculation the loss and accuracy
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
